f62d2aa29514719aa74dd71fd801058e0ab7a3d2
leiber bertel <leiberbertel777@gmail.com>
2025-07-07 21:46:49 -0500

    feat: add provider for consumption to prescription service

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:recipe_book/providers/recipe_provider.dart';
 import 'package:recipe_book/screens/home_screen.dart';
 
 void main() {
@@ -10,10 +12,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hola mundo',
-      home: RecipeBook(),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => RecipesProvider())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Hola mundo',
+        home: RecipeBook(),
+      ),
     );
   }
 }
